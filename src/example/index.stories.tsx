15939119a89ee7909ba57bb1ec1017e9d1d67754
Tony Pinkevych <tony@theuntime.com>
2021-10-02 19:26:53 +0300

    feat: add example

---
+++ src/example/index.stories.tsx
@@ -0,0 +1,18 @@
+import { Story, Meta } from '@storybook/react'
+import React from 'react'
+
+import { Example, IProps } from '.'
+
+export default {
+  title: 'Example',
+  component: Example,
+} as Meta
+
+const Template: Story<IProps> = (args) => (
+  <div style={{ backgroundColor: 'lightgrey', padding: 24 }}>
+    <Example {...args} />
+  </div>
+)
+
+export const Primary = Template.bind({})
+Primary.args = {}
---
+++ src/example/index.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react'
+
+export interface IProps {}
+export const Example: React.FC<IProps> = () => <h1>Hi, I am an example</h1>
